e68f577f4c844abb1263ba31a5f2dbc925d5e47a
Alex <shura.korostelev.93@mail.ru>
2026-05-02 18:39:38 +0300

    Delete Desktop directory

--- Desktop/Дипломы 2026/Воронина/site/apps/calculator/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalculatorConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.calculator"
-    verbose_name = "Калькуляторы"
